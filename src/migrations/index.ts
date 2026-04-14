1933d399f2ea9cf67194370fee236b0111276196
Mutsumi <tolorenqwq@gmail.com>
2026-04-14 10:31:00 +0800

    fix: 重置数据库迁移

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,45 +1,9 @@
-import * as migration_20250929_111647 from './20250929_111647';
-import * as migration_20260410_102102_site_localization from './20260410_102102_site_localization';
-import * as migration_20260410_105153_localized_og_and_media_i18n from './20260410_105153_localized_og_and_media_i18n';
-import * as migration_20260413_020911_cms_publish_flow from './20260413_020911_cms_publish_flow';
-import * as migration_20260413_022212_cms_user_roles_rbac from './20260413_022212_cms_user_roles_rbac';
-import * as migration_20260413_073401_add_card_grid_layout_mode from './20260413_073401_add_card_grid_layout_mode';
-import * as migration_20260413_112130_marketing_content_model_consolidation from './20260413_112130_marketing_content_model_consolidation';
+import * as migration_20260414_022830_initial_schema from './20260414_022830_initial_schema';
 
 export const migrations = [
   {
-    up: migration_20250929_111647.up,
-    down: migration_20250929_111647.down,
-    name: '20250929_111647',
-  },
-  {
-    up: migration_20260410_102102_site_localization.up,
-    down: migration_20260410_102102_site_localization.down,
-    name: '20260410_102102_site_localization',
-  },
-  {
-    up: migration_20260410_105153_localized_og_and_media_i18n.up,
-    down: migration_20260410_105153_localized_og_and_media_i18n.down,
-    name: '20260410_105153_localized_og_and_media_i18n',
-  },
-  {
-    up: migration_20260413_020911_cms_publish_flow.up,
-    down: migration_20260413_020911_cms_publish_flow.down,
-    name: '20260413_020911_cms_publish_flow',
-  },
-  {
-    up: migration_20260413_022212_cms_user_roles_rbac.up,
-    down: migration_20260413_022212_cms_user_roles_rbac.down,
-    name: '20260413_022212_cms_user_roles_rbac',
-  },
-  {
-    up: migration_20260413_073401_add_card_grid_layout_mode.up,
-    down: migration_20260413_073401_add_card_grid_layout_mode.down,
-    name: '20260413_073401_add_card_grid_layout_mode',
-  },
-  {
-    up: migration_20260413_112130_marketing_content_model_consolidation.up,
-    down: migration_20260413_112130_marketing_content_model_consolidation.down,
-    name: '20260413_112130_marketing_content_model_consolidation'
+    up: migration_20260414_022830_initial_schema.up,
+    down: migration_20260414_022830_initial_schema.down,
+    name: '20260414_022830_initial_schema'
   },
 ];
